14b854477ee532523c8b999ec3f81259a60dcf74
Wichert Akkerman <wichert@wiggy.net>
2008-07-19 21:50:33 +0000

    Remove old cruft, switch to GS based installation
    svn path=/Products.PloneOrg/trunk/; revision=21719

--- Products/PloneOrg/Extensions/Install.py
+++
@@ -1,8 +0,0 @@
-from Products.Archetypes.Extensions.utils import install_subskin
-from Products.PloneOrg.config import *
-from StringIO import StringIO
-
-def install(self):
-	out=StringIO()
-	install_subskin(self, out, GLOBALS)
-	return out.getvalue()
--- Products/PloneOrg/__init__.py
+++ Products/PloneOrg/__init__.py
@@ -1,30 +1,5 @@
-from Products.CMFCore.DirectoryView import registerDirectory
 from Products.CMFCore.DirectoryView import registerFileExtension
 from Products.CMFCore.FSFile import FSFile
-from Products.ATContentTypes.permission import permissions
 
-from Products.PloneOrg.config import *
-#import Products.PloneOrg.content
-
-registerDirectory("skins", GLOBALS)
 registerFileExtension("zip", FSFile)
 registerFileExtension("txt", FSFile)
-
-# def initialize(context):
-#     listOfTypes = listTypes(PROJECTNAME)
-#     (content_types, constructors, ftis) = \
-#             process_types(listOfTypes, PROJECTNAME)
-# 
-# 
-#     allTypes = zip(content_types, constructors)
-#     for atype, constructor in allTypes:
-#         kind = "%s: %s" % (PROJECTNAME, atype.archetype_name)
-#         ContentInit(
-#                 kind,
-#                 content_types      = (atype,),
-#                 permission         = permissions[atype.portal_type],
-#                 extra_constructors = (constructor,),
-#                 ).initialize(context)
-# 
-# 
-# 
