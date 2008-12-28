f8f8b68bf18cc7b3b4fd5ba09d63b920dcad045a
Matthew Wilkes <matthew@matthewwilkes.co.uk>
2008-12-28 02:24:02 +0000

    Added script to run archetypes schema migration on all types but PSCFile on the plone.org site
    svn path=/Products.PloneOrg/trunk/; revision=23892

---
+++ updateMostSchemata.py
@@ -0,0 +1,56 @@
+from Products.CMFCore.utils import getToolByName
+from StringIO import StringIO
+
+attool = app['plone.org'].archetype_tool
+
+def manage_updateSchema(self, REQUEST=None, update_all=None,
+                        remove_instance_schemas=None):
+    """Make sure all objects' schema are up to date.
+    """
+    out = StringIO()
+    print >> out, 'Updating schema...'
+
+    update_types = []
+    if REQUEST is None:
+        # DM (avoid persistency bug): avoid code duplication
+        update_types = [ti[0] for ti in self.getChangedSchema() if ti[1]]
+    else:
+        # DM (avoid persistency bug):
+        for t in self._listAllTypes():
+            if REQUEST.form.get(t, False):
+                update_types.append(t)
+        update_all = REQUEST.form.get('update_all', False)
+        remove_instance_schemas = REQUEST.form.get(
+            'remove_instance_schemas', False)
+
+    # XXX: Enter this block only when there are types to update!
+    if update_types:
+        # Use the catalog's ZopeFindAndApply method to walk through
+        # all objects in the portal.  This works much better than
+        # relying on the catalog to find objects, because an object
+        # may be uncatalogable because of its schema, and then you
+        # can't update it if you require that it be in the catalog.
+        catalog = getToolByName(self, 'portal_catalog')
+        portal = getToolByName(self, 'portal_url').getPortalObject()
+        meta_types = [_types[t]['meta_type'] for t in update_types]
+        meta_types.remove("PSCFile")
+        if remove_instance_schemas:
+            func_update_changed = self._removeSchemaAndUpdateChangedObject
+            func_update_all = self._removeSchemaAndUpdateObject
+        else:
+            func_update_changed = self._updateChangedObject
+            func_update_all = self._updateObject
+        if update_all:
+            catalog.ZopeFindAndApply(portal, obj_metatypes=meta_types,
+                search_sub=True, apply_func=func_update_all)
+        else:
+            catalog.ZopeFindAndApply(portal, obj_metatypes=meta_types,
+                search_sub=True, apply_func=func_update_changed)
+        for t in update_types:
+            self._types[t] = _types[t]['signature']
+        self._p_changed = True
+
+    print >> out, 'Done.'
+    return out.getvalue()
+
+print manage_updateSchema(attool)
