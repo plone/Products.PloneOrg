be0e385f3be16ee7f6cd2e599cb5f52844646a47
Wichert Akkerman <wichert@wiggy.net>
2009-01-05 23:54:02 +0000

    now play with several sources dirs
    svn path=/Products.PloneOrg/trunk/; revision=24074

--- mirror.py
+++ mirror.py
@@ -6,16 +6,22 @@ import os
 from os.path import join
 import shutil
 
-SOURCE = '/srv/plone.org/zope/files'
+SOURCES = ('/srv/plone.org/zope/files', '/srv/plone.org/p3/var/files')
 TARGET = '/srv/dist.plone.org/http/root/packages'
 
-for root, dirs, files in os.walk(SOURCE):
-    file, ext = '',''
-    for file in files:
-        ext = os.path.splitext(file)[-1]
-    path = join(root, file)
-    if ext not in ('.egg', '.zip', '.gz', '.tgz'):
-        continue
-    target = join(TARGET, file)
-    print 'Copying %s to %s' % (path, target)
-    shutil.copyfile(path, target)
+for SOURCE in SOURCES:
+    for root, dirs, files in os.walk(SOURCE):
+        file, ext = '', ''
+        for file in files:
+            ext = os.path.splitext(file)[-1]
+        path = join(root, file)
+        if ext not in ('.egg', '.zip', '.gz', '.tgz'):
+            continue
+        target = join(TARGET, file)
+        if os.path.exists(target):
+            continue   
+        print 'Copying %s to %s' % (path, target)
+        shutil.copy2(path, target)
+   
+print 'Done. make sure you fix the rights in %s' % TARGET
+
