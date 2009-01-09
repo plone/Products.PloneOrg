41f2394e91a2eb7c7d4550edd3edcd3975adc406
Alex Clark <aclark@aclark.net>
2009-01-09 18:56:03 +0000

    Commit transaction every 10 times.
    svn path=/Products.PloneOrg/trunk/; revision=24146

--- reindex_products.py
+++ reindex_products.py
@@ -11,8 +11,15 @@ portal = app['plone.org']
 
 utool = portal.portal_url
 brains = portal.portal_catalog.searchResults(portal_type='PSCRelease', path='/plone.org/products')
+count = 0
 for brain in brains:
+    count += 1
     obj = brain.getObject()
     path = '/' + '/'.join(utool.getRelativeContentPath(obj))
     print 'Reindexing %s' % path
     obj.reindexObject(['getCategories', 'getClassifiers', 'getCompatibility'])
+    if count % 10 == 0:
+        print 'Committing...'
+        transaction.commit()
+        print 'Done.'
+    
