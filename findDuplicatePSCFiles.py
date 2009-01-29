f8df0e505d8e4f4798440e32285b1824e3fa8d16
Alex Clark <aclark@aclark.net>
2009-01-29 22:56:00 +0000

    Search the catalog for duplicate PSCFiles, e.g. /foo/bar/20090101.tgz and /foo/baz/20090101.tgz
    svn path=/Products.PloneOrg/trunk/; revision=24731

---
+++ findDuplicatePSCFiles.py
@@ -0,0 +1,25 @@
+import transaction
+from AccessControl.SpecialUsers import system
+from AccessControl.SecurityManagement import newSecurityManager
+from Products.CMFCore.utils import getToolByName
+from Testing.makerequest import makerequest
+
+app=makerequest(app)
+newSecurityManager(None, system)
+
+portal = app['plone.org']
+
+utool = portal.portal_url
+brains = portal.portal_catalog.searchResults(portal_type='PSCFile', path='/plone.org/products')
+count = 0
+for brain in brains:
+    count += 1
+    obj = brain.getObject()
+    path = '/' + '/'.join(utool.getRelativeContentPath(obj))
+    print 'I found %s' % path
+    #obj.reindexObject(['getCategories', 'getClassifiers', 'getCompatibility'])
+    #if count % 10 == 0:
+    #    print 'Committing...'
+    #    transaction.commit()
+    #    print 'Done.'
+    
