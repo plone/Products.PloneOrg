6902e667641f4e2a9027e2d6456eb7a276db68a4
Alex Clark <aclark@aclark.net>
2009-02-11 01:56:48 +0000

    Notes from Tarek
    svn path=/Products.PloneOrg/trunk/; revision=24974

--- findDuplicatePSCFiles.py
+++ findDuplicatePSCFiles.py
@@ -12,14 +12,16 @@ portal = app['plone.org']
 utool = portal.portal_url
 brains = portal.portal_catalog.searchResults(portal_type='PSCFile', path='/plone.org/products')
 count = 0
+
+#17:59 < tarek> brain.getObject().getId()
+#17:59 < tarek> and build a dict
+#17:59 < tarek> wth the ids as keys,
+#17:59 < tarek> using a defaultdict or something
+#18:00 < tarek> if it doesn exist -> dict[key] = [package]
+#18:00 < tarek> if it exists dict[key].append(package2)
+#18:00 < tarek> and all the keys with len > 1 are the bad guys :D
 for brain in brains:
     count += 1
     obj = brain.getObject()
     path = '/' + '/'.join(utool.getRelativeContentPath(obj))
     print 'I found %s' % path
-    #obj.reindexObject(['getCategories', 'getClassifiers', 'getCompatibility'])
-    #if count % 10 == 0:
-    #    print 'Committing...'
-    #    transaction.commit()
-    #    print 'Done.'
-    
