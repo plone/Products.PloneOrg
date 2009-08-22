503c08a948600dfd59ddc1e88be24fa9e0d31e13
Alex Clark <aclark@aclark.net>
2009-08-22 01:59:10 +0000

    Everything but the wf piece.
    svn path=/Products.PloneOrg/trunk/; revision=29260

--- scripts/restrictPoiTrackers.py
+++ scripts/restrictPoiTrackers.py
@@ -1,27 +1,23 @@
+
 import transaction
 from AccessControl.SpecialUsers import system
 from AccessControl.SecurityManagement import newSecurityManager
 from Products.CMFCore.utils import getToolByName
 from Testing.makerequest import makerequest
+from Products.CMFCore.utils import getToolByName
 
 app=makerequest(app)
 newSecurityManager(None, system)
-
 portal = app['plone.org']
+wf = getToolByName(portal, 'portal_workflow')
 
-utool = portal.portal_url
-brains = portal.portal_catalog.searchResults(portal_type='PoiTracker', path='/plone.org/products')
-count = 0
-
-#17:59 < tarek> brain.getObject().getId()
-#17:59 < tarek> and build a dict
-#17:59 < tarek> wth the ids as keys,
-#17:59 < tarek> using a defaultdict or something
-#18:00 < tarek> if it doesn exist -> dict[key] = [package]
-#18:00 < tarek> if it exists dict[key].append(package2)
-#18:00 < tarek> and all the keys with len > 1 are the bad guys :D
+brains = portal.portal_catalog(portal_type='PoiTracker',path='plone.org')
 for brain in brains:
-    count += 1
     obj = brain.getObject()
-    path = '/' + '/'.join(utool.getRelativeContentPath(obj))
-    print '%s' % path
+    print '%s' % '/'.join(obj.getPhysicalPath())
+
+#    review_state = wf.getInfoFor(obj, 'review_state')
+#    if not review_state == 'restricted':
+#        wf.doActionFor(obj,'restrict',wf_id='poi_workflow')
+#        print '    Restricting tracker: %s' %
+#            '/' + '/'.join(portal.getPhysicalPath(obj))
