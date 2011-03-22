14429a6cc67b540031e0c939e7b7118b33b01c33
Chrissy Wainwright <chrissy@sixfeetup.com>
2011-03-22 20:38:39 +0000

    adjustments to the popout login to make sure it works properly refs #11462
    svn path=/plone.org/Products.PloneOrg/trunk/; revision=48181

--- src/Products/PloneOrg/skins/ploneorg/login.js
+++ src/Products/PloneOrg/skins/ploneorg/login.js
@@ -1,7 +1,18 @@
 $(function(){
+    var url = window.location.href;
     $("#login-link").click(function() {
+        // the login template is static html so we define came_from
+        // here as there is no TAL available
+        $('#came-from-input').val(url);
         $("#login-popup").slideToggle();
         return false;
     });
+    $("#login-slide-form").submit(function(){
+    // for testing purposes, we want the form action to point to
+    // /plone.org/login_form if we are local or staging
+    if(url.indexOf('http://plone.org') != 0){
+        $(this).attr('action', '/plone.org/login_form');
+    }
+});
 
 });
