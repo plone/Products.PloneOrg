0ea896cb80c07f33e4d8f22d287d57ce60a25e3d
Myroslav Opyr <myroslav@quintagroup.com>
2011-05-01 01:43:49 +0000

    Correcting typo. Wrong staging buildout configuration file
    svn path=/plone.org/Products.PloneOrg/trunk/; revision=49240

--- fabfile.py
+++ fabfile.py
@@ -19,7 +19,7 @@ def update():
 
 def rebuild():
     with cd(env.code_root):
-        sudo('nice bin/buildout -c buildout-staging.conf', user=env.code_user)
+        sudo('nice bin/buildout -c buildout-staging.cfg', user=env.code_user)
 
 def restart():
     with cd(env.code_root):
